4f76324487579c65185f1a7b6bc00796989b8e2d
Zhey10 <Zhey10@users.noreply.github.com>
2025-02-11 16:26:35 +0100

    SelectItems

--- LoopGame/Assets/Game_Base/Scripts/Equipment/InventoryManager.cs
+++ LoopGame/Assets/Game_Base/Scripts/Equipment/InventoryManager.cs
@@ -45,4 +45,12 @@ public class InventoryManager : MonoBehaviour
             }
         }
     }
+    public void DeseleectAllSlots()
+    {
+        for (int i = 0;i < itemSlot.Length; i++)
+        {
+            itemSlot[i].selectedShader.SetActive(false);
+            itemSlot[i].thisItemSelected = false;
+        }
+    }
 }
--- LoopGame/Assets/Game_Base/Scripts/Equipment/ItemSlot.cs
+++ LoopGame/Assets/Game_Base/Scripts/Equipment/ItemSlot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //Item data
     public string itemName;
@@ -13,16 +14,20 @@ public class ItemSlot : MonoBehaviour
 
     //Item slot
     [SerializeField] private Image itemImage;
-    
+
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
     void Start()
     {
-        
+        inventoryManager = GameObject.Find("GearUI").GetComponent<InventoryManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void AddItem(string itemname, Sprite itemSprite)
     {
@@ -30,5 +35,27 @@ public class ItemSlot : MonoBehaviour
         this.itemSprite = itemSprite;
         isFull = true;
         itemImage.sprite = itemSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+    public void OnLeftClick()
+    {
+        inventoryManager.DeseleectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+    public void OnRightClick()
+    {
+        
     }
 }
