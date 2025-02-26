29c3d964ba32be09597c0e8ef3db0df9c742ab9c
Jorgegiral <jorgegiraldo758@gmail.com>
2025-02-26 18:34:27 +0100

    shopinput

--- LoopGame/Assets/Game_Base/Scripts/Components/FountainRest.cs
+++ LoopGame/Assets/Game_Base/Scripts/Components/FountainRest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FountainRest : MonoBehaviour
 {
@@ -8,26 +9,43 @@ public class FountainRest : MonoBehaviour
     private bool isPlayerinCollider;
     private Animator fountanim;
     public GameObject shop;
-
+    private PlayerInput playerInput;
 
     private void Start()
     {
-        fountanim = GetComponent<Animator>();   
+        fountanim = GetComponent<Animator>();
+        playerInput = FindObjectOfType<PlayerInput>();
     }
-    private void Update()
+    /*private void Update()
     {
         if (isPlayerinCollider && fountanim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
                 fountanim.SetTrigger("Interact");
+                AudioManager.Instance.PlaySFX(6);
                 GameManager.instance.levelCompleted = true;
                 PlayerManager.instance.AddPotions(1);
                 CoinAndScore.instance.AddCoins(15);
                 
             }
         }
-    }
+    }*/
+    public void HandleInteract(InputAction.CallbackContext context)
+    {
+       if (isPlayerinCollider && fountanim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+       {
+           if (context.started)
+           {
+               fountanim.SetTrigger("Interact");
+               AudioManager.Instance.PlaySFX(6);
+               GameManager.instance.levelCompleted = true;
+               PlayerManager.instance.AddPotions(1);
+               CoinAndScore.instance.AddCoins(15);
+
+           }
+       }
+   }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
